c2f072591ee365c9a762cd0dc1a0434160998db4
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-09-13 20:25:55 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  #makes new hash
+  new_hash = {}
+  #makes new hash
+  
+  data.each do |facts, values|
+    #loops facts with values
+     
+    values.each do |value, pigeon_name|
+      #loops through array of names
+      pigeon_name.each do |pigeon|
+        #takes the pigeon_name and pushes into new_hash
+         new_hash[pigeon] ||= {} 
+        new_hash[pigeon][facts] ||= [] 
+        new_hash[pigeon][facts] << value.to_s 
+      end 
+    end 
+  end 
+  new_hash
+  #return new hash
 end
