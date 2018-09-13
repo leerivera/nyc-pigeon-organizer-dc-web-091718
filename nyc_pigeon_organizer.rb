99375b8d30765560387bf2e5734eb3f6e06597fc
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-09-13 20:13:02 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,5 +3,11 @@ def nyc_pigeon_organizer(data)
   data.each do |facts, values|
     values.each do |value, pigeon_name|
       pigeon_name.each do |pigeon|
-         
+         new_hash[pigeon] ||= {} 
+        new_hash[pigeon][facts] ||= [] 
+        new_hash[pigeon][facts] << value.to_s 
+      end 
+    end 
+  end 
+  new_hash
 end
